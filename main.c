0fe581abe967893577590dc24d3ad0f8e5e80c27
indy <12000107@student.pxl.be>
2021-04-30 14:07:59 +0200

    Revert "update"
    This reverts commit fd1d7b183beff846fc265a6a9ee91dd9dd062457.

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define __DEBUG
-
+//#define __DEBUG
 #define BMPINPUTFILE "test.bmp"
+
+void KleurBits();
 
 int main()
 {
-#ifdef __DEBUG
-    printf("DEBUG info: BMP transformer\n");
-#endif
+	void KleurBits();
+    #ifdef __DEBUG
+        printf("DEBUG info: BMP transformer\n");
+    #endif
 
     FILE* inputFilePointer = fopen(BMPINPUTFILE, "rb"); //maak een file pointer naar de afbeelding
     if(inputFilePointer == NULL) //Test of het open van de file gelukt is!
@@ -18,9 +20,9 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-#ifdef __DEBUG
-    printf("DEBUG info: Opening File OK: %s\n", BMPINPUTFILE);
-#endif
+    #ifdef __DEBUG
+        printf("DEBUG info: Opening File OK: %s\n", BMPINPUTFILE);
+    #endif
 
     unsigned char bmpHeader[54]; // voorzie een array van 54-bytes voor de BMP Header
     fread(bmpHeader, sizeof(unsigned char), 54, inputFilePointer); // lees de 54-byte header
@@ -30,29 +32,28 @@ int main()
     int breedte = *(int*)&bmpHeader[18];
     int hoogte = *(int*)&bmpHeader[22];
 
-#ifdef __DEBUG
-    printf("DEBUG info: breedte = %d\n", breedte);
-    printf("DEBUG info: hoogte = %d\n", hoogte);
-#endif
+    #ifdef __DEBUG
+        printf("DEBUG info: breedte = %d\n", breedte);
+        printf("DEBUG info: hoogte = %d\n", hoogte);
+    #endif
 
     int imageSize = 3 * breedte * hoogte; //ieder pixel heeft 3 byte data: rood, groen en blauw (RGB)
     unsigned char* inputPixels = (unsigned char *) calloc(imageSize, sizeof(unsigned char)); // allocate een array voor alle pixels
+	
 
-
-
+	
     fread(inputPixels, sizeof(unsigned char), imageSize, inputFilePointer); // Lees alle pixels (de rest van de file
     fclose(inputFilePointer);
-    for(int i =0; i < imageSize-2; i+=3)
-    {
-        printf("pixel %d: B= %d, G=%d, R=%d\n", i, inputPixels[i], inputPixels[i+1], inputPixels[i+2]);
-    }
-
+	for(int i =0; i < imageSize-2; i+=3)
+	{
+		printf("pixel %d: B= %d, G=%d, R=%d\n", i, inputPixels[i], inputPixels[i+1], inputPixels[i+2]);
+	}
+   
     fclose(inputFilePointer);
     free(inputPixels);
-
+    
     return 0;
 }
-
 
 
 void KleurBits(){
